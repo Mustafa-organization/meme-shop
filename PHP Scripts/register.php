bfde18c165cf71edf57e627bd95255cd91ccce63
Weisz Tibor <weisztibor1@gmail.com>
2018-11-22 19:42:48 +0100

    register database

---
+++ PHP Scripts/register.php
@@ -0,0 +1,17 @@
+<?php
+$fullname=$_Post['name'];
+$registeruname=$_Post['nuname'];
+$registerpsw=$_Post['npsw'];
+$registerpswa=$_Post['pswa'];
+$news=$_Post['news'];
+$addressname=$_Post['addressname'];
+$addressnumber=$_Post['addressnumber'];
+
+
+
+$dbc=mysqli_connect("localhost",'root','','memeShop');
+$sql = mysql_query(INSERT INTO Customers (CustomerName,Customerusername ,News, Addressname,Addressnumber ,Password)
+VALUES (:fullname, :registerusername, ,:news ,:addressname,:addressnumber, :registerpsw));
+
+ mysqli_close($dbc);
+ ?>
